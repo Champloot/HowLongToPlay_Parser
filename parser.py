bdcbd52ce1bf6db9e0ffd0f88bb0c18ab8aa0db7
Artem Verchenko <Artem.avert@yandex.ru>
2025-03-13 19:03:10 +0300

    Парсер готов вроде

--- parser.py
+++ parser.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
 # Vars
 game = "dead-cells"
@@ -10,6 +11,21 @@ response_status = response.status_code
 text_from_page = response.text
 soup = BeautifulSoup(text_from_page, 'lxml')
 
-block = soup.find_all('div', class_="game__main-time-item")
+arr = []
 
-print(block)
+first_block = soup.find('div', class_="game__main-time-item")
+second_block = first_block.find_next('div', class_="game__main-time-item")
+third_block = second_block.find_next('div', class_="game__main-time-item")
+
+arr = [first_block.text, second_block.text, third_block.text]
+
+# Main campaign
+main_campaign = re.findall(r'\d+', arr[0])
+
+# Campaign + additional tasks
+campaign_and_add_tasks = re.findall(r'\d+', arr[1])
+
+# Full playthrough
+full_playthrough = re.findall(r'\d+', arr[2])
+
+print(main_campaign, campaign_and_add_tasks, full_playthrough)
