95bb6ba6c705729ae1eccf5b3d166c3b985bfaec
Artem Verchenko <Artem.avert@yandex.ru>
2025-03-13 19:41:01 +0300

    Парсер готов теперь наверняка

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from parser import parse
+
 def data_base_update():
     pass
 
-
 if __name__ == "__main__":
-    pass
+    data_base_update()
+    game_name = input()
+    print(parse(game_name))
--- parser.py
+++ parser.py
@@ -2,30 +2,32 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-# Vars
-game = "dead-cells"
-link = f"https://howlongtoplay.ru/games/{game}"
+def parse(game="dead-cells") -> list:
+    # Vars
+    link = f"https://howlongtoplay.ru/games/{game}"
 
-response = requests.get(link)
-response_status = response.status_code
-text_from_page = response.text
-soup = BeautifulSoup(text_from_page, 'lxml')
+    response = requests.get(link)
+    response_status = response.status_code
+    if not response_status:
+        return 0
+    text_from_page = response.text
+    soup = BeautifulSoup(text_from_page, 'html.parser')
 
-arr = []
+    first_block = soup.find('div', class_="game__main-time-item")
+    second_block = first_block.find_next('div', class_="game__main-time-item")
+    third_block = second_block.find_next('div', class_="game__main-time-item")
 
-first_block = soup.find('div', class_="game__main-time-item")
-second_block = first_block.find_next('div', class_="game__main-time-item")
-third_block = second_block.find_next('div', class_="game__main-time-item")
+    arr = [first_block.text, second_block.text, third_block.text]
 
-arr = [first_block.text, second_block.text, third_block.text]
+    # Main campaign
+    main_campaign = re.findall(r'\d+', arr[0])
 
-# Main campaign
-main_campaign = re.findall(r'\d+', arr[0])
+    # Campaign + additional tasks
+    campaign_and_add_tasks = re.findall(r'\d+', arr[1])
 
-# Campaign + additional tasks
-campaign_and_add_tasks = re.findall(r'\d+', arr[1])
+    # Full playthrough
+    full_playthrough = re.findall(r'\d+', arr[2])
 
-# Full playthrough
-full_playthrough = re.findall(r'\d+', arr[2])
+    return [main_campaign, campaign_and_add_tasks, full_playthrough]
 
-print(main_campaign, campaign_and_add_tasks, full_playthrough)
+# print(parse())
