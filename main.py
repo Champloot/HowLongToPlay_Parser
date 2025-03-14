a82b2e241b5438e7f5af8041d0512d7b2f0fef37
Artem Verchenko <Artem.avert@yandex.ru>
2025-03-14 14:07:31 +0300

    Console args and new .gitignore

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
+from math import gamma
+
 from parser import parse
+import optparse
 
 def data_base_update():
     pass
 
 if __name__ == "__main__":
     data_base_update()
-    game_name = input()
+
+    console_option_parser = optparse.OptionParser()
+    console_option_parser.add_option("-g", "--game",
+                                     dest="game",
+                                     help="Name of game")
+    (option, arguments) = console_option_parser.parse_args()
+    game_name = option.game
     print(parse(game_name))
