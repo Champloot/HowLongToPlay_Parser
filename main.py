d6e443ffd6a84fed9433b6495230443ae7374fcc
Artem Verchenko <Artem.avert@yandex.ru>
2025-03-27 00:40:50 +0300

    bug fix

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import optparse
+import argparse
 import json
 import os
-from time import sleep
 
 from another_parser import DefaultError, parse
 
@@ -17,6 +16,7 @@ def base_check():
 
 
 def json_operation(game, code=None):
+    base_check()
     with open('data.json', 'r') as file:
         data = json.load(file)
 
@@ -24,19 +24,20 @@ def json_operation(game, code=None):
         return data[game]
     else:
         if code is None:
-            raise DefaultError("v base net infi")
+            raise DefaultError("V base net infi")
         data[game] = code
 
         with open('data.json', 'w') as file:
             json.dump(data, file, indent=4)
 
 
-def data_update():
-    cnt = 1
+def data_update() -> str:
+    base_check()
+    cnt = 8000
     err_cnt = 0
-    while err_cnt < 100:
+    while err_cnt < 1000:
         with open('data.json', 'r') as file:
-            data : dict = json.load(file)
+            data: dict = json.load(file)
         if not (cnt in data.values()):
             try:
                 name = parse(game_code=cnt, target='name')[0]
@@ -44,34 +45,35 @@ def data_update():
                     data[name] = cnt
                     json.dump(data, file, indent=4)
                 err_cnt = 0
-            except DefaultError as e:
+            except DefaultError:
                 err_cnt += 1
                 cnt += 1
 
         cnt += 1
+    return "Base was updated"
 
 
 if __name__ == "__main__":
-    console_option_parser = optparse.OptionParser()
-    console_option_parser.add_option("-g", "--game",
-                                     dest="game",
-                                     help="Name of game")
-    console_option_parser.add_option("-u", "--update",
-                                     dest="update",
-                                     help="Update the base")
+    console_option_parser = argparse.ArgumentParser()
+    console_option_parser.add_argument("-g", "--game",
+                                       action="store",
+                                       type=str,
+                                       dest="game",
+                                       help="Name of game")
+    console_option_parser.add_argument("-u", "--update",
+                                       action="store_true",
+                                       dest="update",
+                                       default=False,
+                                       help="Update the base")
 
-    (option, arguments) = console_option_parser.parse_args()
+    args = console_option_parser.parse_args()
+    if args.game:
+        game_name: str = args.game
 
-    try:
-        base_check()
-        if option.game: game_name: str = option.game
-        elif option.update:
-            data_update()
-            exit()
         game_id = json_operation(game_name)
         ans_from_parser = parse(game_id)
         print(f"Main campaign:                  ~{ans_from_parser[1]}h.\n"
               f"Campaign + additional tasks:    ~{ans_from_parser[2]}h.\n"
               f"Full playthrough:               ~{ans_from_parser[3]}h.\n")
-    except DefaultError as exception:
-        print("Something went wrong")
+    elif args.update:
+        print(data_update())
